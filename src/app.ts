286b033412a08c630ad165762441993a3b41facb
Harry Craven <harrycraven.dev@gmail.com>
2022-08-25 17:31:14 -0600

    feature/error-handler-and-route-not-found

--- src/app.ts
+++ src/app.ts
@@ -3,35 +3,41 @@ import bodyParser from "body-parser";
 import { RegisterRoutes } from "../build/routes";
 import helmet from "helmet";
 import compression from "compression";
+import errorHandler from "./utils/error-handling/errorHandler";
+import notFoundHandler from "./utils/not-found-handler/notFoundHandler";
 
 const run = async () => {
-	const app = express();
+  const app = express();
 
-	try {
-		// Use body parser to read sent json payloads
-		app.use(
-			bodyParser.urlencoded({
-				extended: true,
-			})
-		);
+  try {
+    // Use body parser to read sent json payloads
+    app.use(
+      bodyParser.urlencoded({
+        extended: true,
+      })
+    );
 
-		app.use(bodyParser.json());
+    app.use(bodyParser.json());
 
-		app.use(helmet());
+    app.use(helmet());
 
-		app.use(compression());
+    app.use(compression());
 
-		RegisterRoutes(app);
-	} catch (error) {
-		console.log("Process exited with error: ", error);
-		process.exit(1);
-	}
+    app.use(notFoundHandler);
 
-	const port = process.env.PORT || 4500;
+    RegisterRoutes(app);
 
-	app.listen(port, () =>
-		console.log(`Example app listening at http://localhost:${port}`)
-	);
+    app.use(errorHandler);
+  } catch (error) {
+    console.log("Process exited with error: ", error);
+    process.exit(1);
+  }
+
+  const port = process.env.PORT || 4500;
+
+  app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`)
+  );
 };
 
 run();
